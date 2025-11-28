7bfc8b49c890f55b73794d5a809c6307f0124f86
kaduFranca <kadugabrielmdf@gmail.com>
2025-11-27 22:57:58 -0300

    adicionado layout inicial

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,50 @@
-function Home() {
+import { AppShell, NavLink, Burger, Group, Text } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { Home as HomeIcon, Users, Settings } from "lucide-react";
+
+export default function Home({ children }) {
+  const [opened, { toggle }] = useDisclosure();
+
   return (
-    <>
-      <p>Home</p>
-    </>
+    <AppShell
+    layout="alt"
+      header={{ height: 60 }}
+      navbar={{
+        width: 240,
+        breakpoint: "sm",
+        collapsed: { mobile: false },
+      }}
+      padding="md"
+    >
+      {/* HEADER */}
+      <AppShell.Header>
+        <Group h="100%" px="md">
+          <Text fw={600} size="lg">
+            Meu SaaS
+          </Text>
+        </Group>
+      </AppShell.Header>
+
+      {/* SIDEBAR */}
+      <AppShell.Navbar p="md">
+        <NavLink
+          label="Dashboard"
+          leftSection={<HomeIcon size={18} />}
+        />
+        <NavLink
+          label="Clientes"
+          leftSection={<Users size={18} />}
+        />
+        <NavLink
+          label="Configurações"
+          leftSection={<Settings size={18} />}
+        />
+      </AppShell.Navbar>
+
+      {/* CONTEÚDO */}
+      <AppShell.Main>
+        {children}
+      </AppShell.Main>
+    </AppShell>
   );
 }
-
-export default Home;
